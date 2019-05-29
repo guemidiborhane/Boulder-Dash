05443feb9abe10eed2a3289196a3df22928fe71c
Borhaneddine GUEMIDI <guemidiborhane@gmail.com>
2019-05-29 23:40:50 +0100

    No violence, gentlemen -- no violence, I beg of you!  Consider the furniture! 		-- Sherlock Holmes

---
+++ world/src/main/java/world/elements/entity/IMovement.java
@@ -0,0 +1,55 @@
+package world.elements.entity;
+
+import world.elements.IAction;
+
+import java.util.Optional;
+
+
+public interface IMovement extends IAction {
+    /**
+     * To go entity right
+     * */
+    void goRight();
+    /**
+     * To go entity left
+     * */
+    void goLeft();
+    /**
+     * To go entity up
+     * */
+    void goUp();
+    /**
+     * To go entity down
+     * */
+    void goDown();
+    /**
+     * To go entity waiting
+     * */
+    void goRest();
+    /**
+     * @return the stateManager
+     * @see StateManager
+     * @see StateType
+     * @see State
+     * */
+    StateManager getStateManager();
+    /**
+     * isEmpty
+     * check if the specified coordinates are fulled
+     * @param xCase Integer
+     * @param yCase Integer
+     * @return boolean
+     * */
+    boolean isEmpty(int xCase, int yCase);
+    /**
+     * Allow to ingnore the entity's behavior
+     * */
+    void ignoreBehavior();
+    /**
+     * hasFinished
+     * check is the element had finished it current movement
+     * @return boolean
+     * */
+    boolean hasFinish();
+
+}
